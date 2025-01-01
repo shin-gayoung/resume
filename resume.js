e768e08ec524a03e1a5a80407beaa01d9180f625
shin-gayoung <48895173+shin-gayoung@users.noreply.github.com>
2025-01-02 08:10:09 +0900

    Update resume.js

--- resume.js
+++ resume.js
@@ -2,16 +2,17 @@
 alert('모바일 최적화 페이지입니다. 모바일 화면으로 열람하시길 권장 드립니다.')
 
 function messageMail() {
-    alert('제 메일 주소가 클립보드에 복사되었습니다. (rkdud12547@gmail.com) 본 메일로 문의 부탁드립니다. :)');
+    alert('메일 주소가 클립보드에 복사되었습니다. (rkdud12547@gmail.com) 본 메일로 문의 부탁드립니다. :)')
     navigator.clipboard.writeText('rkdud12547@gmail.com')
   }
 
   function messageInterview() {
-    alert('면접일 예약 기능이 준비중입니다. 제 메일 주소가 클립보드에 복사되었습니다. (rkdud12547@gmail.com) 본 메일로 문의 부탁드립니다. :)');
+    alert('면접일 예약 기능이 준비중입니다. 메일 주소가 클립보드에 복사되었습니다. (rkdud12547@gmail.com) 본 메일로 문의 부탁드립니다. :)')
     navigator.clipboard.writeText('rkdud12547@gmail.com')
   }
 
   function changeIconLikeOn() {
+    alert('찜하기가 완료되었습니다')
     const icon_like = document.getElementById('icon_like')
     const btnLike =document.getElementById('btnLike')
     icon_like.src ='./custom/icon_like_on.svg'
